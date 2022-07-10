56a92438a0f4dbb03805f15621fa7d56b1d35157
edwin <emash90@gmail.com>
2022-07-10 15:14:43 +0300

    fixing deploying to heroku

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use("/api/package", require("./routes/packageRoutes"));
 app.use("/api/delivery", require("./routes/deliveryRoutes"));
 app.get("*", (req, res) =>
     res.sendFile(
-        path.resolve(__dirname, "./", "client", "build", "index.html")
+        path.resolve(__dirname, "../", "client", "build", "index.html")
     )
 );
 if (process.env.NODE_ENV === "production") {
