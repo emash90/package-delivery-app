a871b6b1c31c370ec30e044fcc09ca8870f9f29a
edwin <emash90@gmail.com>
2022-07-10 14:45:53 +0300

    fixing deploying to heroku

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ const { errorHandler } = require('./middlewares/errorMiddleware')
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PATCH", "DELETE"],
+        origin: "*",
+        methods: ["GET", "POST"]
     },
 })
 io.on("connection", (socket) => {
