9047522b2ba9835e5c5ce62040b72ad2a0d0085c
edwin <emash90@gmail.com>
2022-07-10 12:47:36 +0300

    fixing deploying issues

--- backend/app.js
+++ backend/app.js
@@ -31,11 +31,16 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "../client/build")));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/package", require("./routes/packageRoutes"));
 app.use("/api/delivery", require("./routes/deliveryRoutes"));
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+} else {
+    app.get("/", (req, res) => {
+        res.send('set to "production"');
+    });
+}
 //app.use(errorHandler);
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
